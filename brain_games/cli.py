ac7f52e96ae46037289ceb0b9f60dfceef2eb5b0
tpl-86 <tron-050686@mail.ru>
2023-12-17 18:04:19 +0300

    Add cli.py

--- brain_games/cli.py
+++ brain_games/cli.py
@@ -2,6 +2,6 @@ import prompt
 
 
 def welcome_user():
-    name = pormpt.string('May I have your name?')
+    name = prompt.string('May I have your name? ')
     print(f'Hello, {name}!')
 
--- brain_games/scripts/brain_games.py
+++ brain_games/scripts/brain_games.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 
+from brain_games import cli
+
+
 def greet():
     print('Welcome to the Brain Games!')
 
@@ -10,6 +13,7 @@ def main():
 
 if __name__ == '__main__':
     main()
+    cli.welcome_user()
 
 
 
