5be5a822308ce8adfbad7c6931492a3e0a9beadd
tpl-86 <tron-050686@mail.ru>
2024-01-10 15:40:47 +0300

    Add games brain-gcd and brain-progression

---
+++ brain_games/games/progression_game.py
@@ -0,0 +1,43 @@
+import random
+
+
+rules = 'What number is missing in the progression?'
+
+
+def progression_output():
+    result = []
+    for i in range(3):
+        start = random.randint(0, 100)
+        step = random.randint(1, 11)
+        progression_length = random.randint(5, 10)
+        result_progression = []
+        for j in range(progression_length):
+            result_progression.append(start + j * step)
+        result.append(result_progression)
+    return result
+
+
+def generates_question(list_progressions, items):
+    result = []
+    for i in range(len(list_progressions)):
+        result_progression = ''
+        for j in range(len(list_progressions[i])):
+            if list_progressions[i][j] == items[i]:
+                result_progression += '..' + ' '
+            else:
+                result_progression += str(list_progressions[i][j]) + ' '
+        result.append(result_progression)
+    return result
+
+
+def correct_answer(list_progressions):
+    result = []
+    for progression in list_progressions:
+        item = random.randrange(0, len(progression) - 1)
+        result.append(progression[item])
+    return result
+
+
+progressions = progression_output()
+answer = correct_answer(progressions)
+question = generates_question(progressions, answer)
